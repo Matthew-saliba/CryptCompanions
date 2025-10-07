daf7e704805a70a703ad65e9707c1e946359372c
Matthew-saliba <matthewsaliba39@gmail.com>
2025-10-07 12:12:13 +0200

    Added Shop functionality
    Added shop script for the player to purchase arrows and flasks

---
+++ Crypt Companions/Assets/Scripts/ArrowShop.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ArrowShop : ShopTile
+    {
+        protected override void TryPurchase()
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            Player playerScript = player.GetComponent<Player>();
+            PlayerRanged playerRanged = player.GetComponent<PlayerRanged>();
+            if (playerScript.GetGold() >= itemCost)
+            {
+                playerScript.AddGold(-itemCost);
+                playerRanged.AddArrows(30);
+                Debug.Log("Arrows purchased!");
+            }
+            else
+            {
+                Debug.Log("Not enough gold to purchase arrows.");
+            }
+        }
+    }
+}
